39f6029f455b575fdcf9ca0edacd2ff3e7861441
yoon <parkyoon97@gmail.com>
2023-03-06 01:18:23 +0000

    Update 9 files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import "./styles.css";
+import React from "react";
 
-export default function App() {
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+
+function App() {
+  const dummyMovies = [
+    {
+      id: 1,
+      title: "Some Dummy Movie",
+      openingText: "This is the opening text of the movie",
+      releaseDate: "2021-05-18",
+    },
+    {
+      id: 2,
+      title: "Some Dummy Movie 2",
+      openingText: "This is the second opening text of the movie",
+      releaseDate: "2021-05-19",
+    },
+  ];
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={dummyMovies} />
+      </section>
+    </React.Fragment>
   );
 }
+
+export default App;
